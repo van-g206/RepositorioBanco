b8e4d0f3bd10f96e93275dc8e271cd4c8edc1f7a
van-g206 <148474358+van-g206@users.noreply.github.com>
2024-04-08 19:59:35 -0300

    Add files via upload

---
+++ ProjetoBanco/ClasseConta.java
@@ -0,0 +1,13 @@
+
+public class ClasseConta {
+
+   short atributoNumeroConta; // 0
+   float atributeSaldoConta; // 0.0
+   String atributetipoConta; // null
+   
+   
+   String atributoNomeCliente; // null
+   int atributoDocumentoCliente; // 0 
+   
+
+}
---
+++ ProjetoBanco/ClassePrincipal.java
@@ -0,0 +1,24 @@
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		
+		ClasseConta objeto_conta_um; // Criou a váriável
+		objeto_conta_um = new  ClasseConta(); // Instanciou o objeto.
+		
+		ClasseConta objeto_conta_dois = new ClasseConta(); // Instalou a váriável.
+		
+		System.out.println(objeto_conta_um.atributoNumeroConta);
+		System.out.println(objeto_conta_um.atributoSaldoConta);
+		System.out.println(objeto_conta_um.atributoTipoConta);
+		System.out.println(objeto_conta_um.atributoNomeCliente);
+		System.out.println(objeto_conta_um.atributoDocumentoCliente);
+		
+		
+		
+		
+		 
+
+	}
+
+}
